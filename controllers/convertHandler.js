5ae1f1a6cf93474e1c918fcb4f54a4b34c356db8
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2020-03-12 14:32:09 +0600

    added converstion features

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -8,32 +8,64 @@
 
 function ConvertHandler() {
 	this.getNum = function(input) {
-		let result;
+		const regex = new RegExp(/[a-zA-Z]+/);
+		const fracRegex = new RegExp(/\//);
 
-		return result;
+		// let num = input.split(regex)[0];
+
+		let num = input.split(regex)[0] || 1;
+
+		if (fracRegex.test(num)) {
+			return this.getFractionalNum(num);
+		} else {
+			return Number(num);
+		}
 	};
 	this.getFractionalNum = function(input) {
-		let result;
-
-		return result;
+		let result = input.split('/');
+		if (result.length > 2) return 'invalid number';
+		else {
+			return result[0] / result[1];
+		}
 	};
 
 	this.getUnit = function(input) {
-		let result;
+		const regex = new RegExp(/[a-zA-Z]+/);
 
-		return result;
+		const unit = input.match(regex)[0];
+		const validUnit = [
+			'gal',
+			'l',
+			'mi',
+			'km',
+			'lbs',
+			'kg',
+			'GAL',
+			'L',
+			'MI',
+			'KM',
+			'LBS',
+			'KG',
+		];
+		if (!validUnit.includes(unit)) {
+			return 'invalid unit';
+		}
+
+		return unit.toLowerCase();
 	};
 
 	this.getReturnUnit = function(initUnit) {
-		let result;
+		const input = ['gal', 'l', 'mi', 'km', 'lbs', 'kg'];
+		const expect = ['l', 'gal', 'km', 'mi', 'kg', 'lbs'];
 
-		return result;
+		return expect[input.indexOf(initUnit)];
 	};
 
 	this.spellOutUnit = function(unit) {
-		let result;
+		const input = ['gal', 'l', 'mi', 'km', 'lbs', 'kg'];
+		const expect = ['gallon', 'litre', 'miles', 'kilometers', 'kilograms', 'pound'];
 
-		return result;
+		return expect[input.indexOf(unit)];
 	};
 
 	this.convert = function(initNum, initUnit) {
@@ -41,14 +73,20 @@ function ConvertHandler() {
 		const lbsToKg = 0.453592;
 		const miToKm = 1.60934;
 		let result;
-
+		if (initUnit === 'gal' || initUnit === 'l') {
+			return initUnit === 'gal' ? (result = initNum * galToL) : (result = initNum / galToL);
+		}
+		if (initUnit === 'lbs' || initUnit === 'kg') {
+			return initUnit === 'lbs' ? (result = initNum * lbsToKg) : (result = initNum / lbsToKg);
+		}
+		if (initUnit === 'mi' || initUnit === 'km') {
+			return initUnit === 'mi' ? (result = initNum * miToKm) : (result = initNum / miToKm);
+		}
 		return result;
 	};
 
 	this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-		let result;
-
-		return result;
+		return `${initNum}${initUnit} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
 	};
 }
 
--- routes/api.js
+++ routes/api.js
@@ -16,12 +16,19 @@ module.exports = function(app) {
 
 	app.route('/api/convert').get(function(req, res) {
 		const input = req.query.input;
+
 		const initNum = convertHandler.getNum(input);
 		const initUnit = convertHandler.getUnit(input);
 		const returnNum = convertHandler.convert(initNum, initUnit);
 		const returnUnit = convertHandler.getReturnUnit(initUnit);
 		const toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-		//res.json
+		res.json({
+			initNum,
+			initUnit,
+			returnNum,
+			returnUnit,
+			string: toString,
+		});
 	});
 };
